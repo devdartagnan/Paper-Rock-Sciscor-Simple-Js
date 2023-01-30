9587ba8516a0249ae6f9b94b6186085ee584601a
Leozinsk <leozinsk@gmail.com>
2023-01-30 05:22:06 +0000

    adding scores

--- hand.js
+++ hand.js
@@ -2,21 +2,30 @@ const hands = document.querySelectorAll('.hand')
 const resultDiv = document.querySelector('.result-text')
 const resultImg = document.querySelector('.result-img')
 const resultSection = document.querySelector('.result')
+const p1Score = document.querySelector('.p1-score')
+const cpuScore = document.querySelector('.cpu-score')
 
+//counters
+let p1Counter = 0
+let cpuCounter = 0
 let n = 0
+
+//checking img
 const rs = resultImg.src.slice(-1)
 rs === '#' ? resultSection.classList.add('none') : resultSection.classList.remove('none')
 
+//click loop
 hands.forEach(() => addEventListener('click', (e) => {
     const click = e.target
     n === 0 ? combat(click.alt) : void (0)
     setTimeout(() => {
         n = 0
-    }, 1000
+    }, 200
     )
 }))
 
 function combat(hand) {
+
     const array = ['paper', 'rock', 'scisor']
     const cpu = ['rock', 'scisor', 'paper']
     let cpuHand = Math.floor(Math.random() * 3)
@@ -27,11 +36,15 @@ function combat(hand) {
             resultImg.src = `assets/${cpu[cpuHand]}.png`
             resultDiv.textContent = `Congrats you won!`
             resultSection.classList.remove('none')
+            p1Counter += 1
+            p1Score.textContent = p1Counter
             n = 1
         } else {
             resultImg.src = `assets/${cpu[cpuHand]}.png`
             resultDiv.textContent = `Sorry you lost!`
             resultSection.classList.remove('none')
+            cpuCounter += 1
+            cpuScore.textContent = cpuCounter
             n = 1
         }
     }
