e7258f971b3ccbab4da17afa3650484df60d6845
Leozinsk <leozinsk@gmail.com>
2023-01-30 04:45:17 +0000

    first commit

---
+++ hand.js
@@ -0,0 +1,44 @@
+const hands = document.querySelectorAll('.hand')
+const resultDiv = document.querySelector('.result-text')
+const resultImg = document.querySelector('.result-img')
+const resultSection = document.querySelector('.result')
+
+let n = 0
+const rs = resultImg.src.slice(-1)
+rs === '#' ? resultSection.classList.add('none') : resultSection.classList.remove('none')
+
+hands.forEach(() => addEventListener('click', (e) => {
+    const click = e.target
+    n === 0 ? combat(click.alt) : void (0)
+    setTimeout(() => {
+        n = 0
+    }, 1000
+    )
+}))
+
+function combat(hand) {
+    const array = ['paper', 'rock', 'scisor']
+    const cpu = ['rock', 'scisor', 'paper']
+    let cpuHand = Math.floor(Math.random() * 3)
+    const result = array[cpuHand]
+
+
+    console.log(cpu[cpuHand])
+    console.log(hand, 'hand')
+    if (hand !== undefined) {
+        if (hand === result) {
+            resultImg.src = `assets/${cpu[cpuHand]}.png`
+            resultDiv.textContent = `Congrats you won!`
+            resultSection.classList.remove('none')
+            n = 1
+        } else {
+            resultImg.src = `assets/${cpu[cpuHand]}.png`
+            resultDiv.textContent = `Sorry you lost!`
+            resultSection.classList.remove('none')
+            n = 1
+        }
+    }
+}
+
+
+//<a href="https://www.vecteezy.com/free-vector/cartoon">Cartoon Vectors by Vecteezy</a>
